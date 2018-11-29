22242111ddc4277a289a7e19852b4849dfe43f59
ankaziele <ankaziele@gmail.com>
2018-11-29 19:05:32 +0100

    adds counter with increment on click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Counter from "./components/Counter"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hello world!!!</h1>
+        <Counter />
       </div>
     );
   }
